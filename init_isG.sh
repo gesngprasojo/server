90f2b432adec213600685aae91953ffc9a886aa2
Gesang Prasojo <gesangprasojo@gmail.com>
2022-01-16 23:10:50 +0700

    init

---
+++ init_isG.sh
@@ -0,0 +1,11 @@
+function run(){
+sudo yum install git -y
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+}
+
