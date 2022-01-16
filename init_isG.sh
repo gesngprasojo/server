7958129e2d6a9a801a82466610fb5f17a19e371d
Gesang Prasojo <gesangprasojo@gmail.com>
2022-01-17 01:23:51 +0700

    oke

--- init_isG.sh
+++ init_isG.sh
@@ -7,5 +7,9 @@ sudo yum-config-manager \
 
 sudo yum install docker-ce docker-ce-cli containerd.io -y
 sudo systemctl start docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/v1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 }
 
